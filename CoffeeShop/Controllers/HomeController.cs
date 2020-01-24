b29db41332061d580743c6504a6aaf059f9c55e3
The Bonitati Collective <mbon419@gmail.com>
2020-01-23 21:34:59 -0500

    first commit

---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // need one action to load our RegistrationPage, also need a view
+        // need one action to take those user inputs and display the user name, in a new view
+        public IActionResult Registration(Registerer registerer)
+        {
+            // if no view is specificed it defaults to the Action Name
+            return View(registerer);
+        }
+
+        public IActionResult Registered(Registerer registerer)
+        {
+            return View(registerer);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoffeeShop/Models/Registerer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models
+{
+    public partial class Registerer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int? PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
